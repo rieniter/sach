f967a9e65367e1f9fbc26d2ecf892609f5960ea7
Bach <34160378+bach@users.noreply.github.com>
2017-12-01 19:03:23 +0700

    Initial commit

---
+++ admin/include/include.php
@@ -0,0 +1,7 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$dbname = "id3169149_dbsach";
+	$mysqli = mysqli_connect($host,$user,$pass,$dbname) or die(mysqli_connect_errno());
+?>
